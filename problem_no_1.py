2685a87638dc2679b5ac588589aa2addf9b77b27
arifulislamnayan <farazi.aarif@gmail.com>
2015-02-01 11:55:37 +0600

    problem no.1 solution added

---
+++ problem_no_1.py
@@ -0,0 +1,17 @@
+def sleep_in(weekday,vacation):
+	if not weekday and vacation:
+		return True
+
+	else:
+		return False
+
+
+
+def sleep_in(weekday, vacation):
+	if weekday and not vacation:
+		return False
+
+	else:
+		return True
+
+		
